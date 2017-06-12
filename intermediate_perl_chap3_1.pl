44ad471cebd560853fe35033396cc4ecdaaa3a2c
Ugrankar <pritesh.ugrankar@gmail.com>
2017-06-12 19:48:41 +0530

    new scripts

---
+++ intermediate_perl_chap3_1.pl
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+
+sub check_required {
+	my $who = shift;
+	my %whos_items = map { $_, 1 } @_;
+	my @missing_items;
+	my @common_items;
+	my @extra_items;
+
+	my @required = qw(sunscreen preserver jacket water_bottle);
+	my %required_items = map { $_, 1 } @required;
+
+	foreach my $item (@required) {
+		if (not exists $whos_items{$item}) {
+			push @missing_items, $item; 
+		}else {
+			push @common_items, $item;
+		}
+	}
+
+	foreach my $extra (@_) {
+		if (not exists $required_items{$extra}) {
+			push @extra_items, $extra;
+		}
+	}
+	print "Missing Items:  @missing_items\n";
+	print "Common  Items:  @common_items\n";
+	print "Extra   Items:  @extra_items\n";
+	
+}
+
+my @gilligan = qw(red_shirt hat sunscreen preserver goggles beer);
+check_required("gilligan", @gilligan);
--- test.pl
+++ test.pl
@@ -1,21 +1,16 @@
 use strict;
 use warnings;
 
-my @arr = (10, 20, 0b1101, 0xF, 023, 023, 10, 20, -1, 0xF, 32.5);
-#my @converted = map {$_ =~ /^-?0/? oct ($_): $_}  @arr;
+my @castaways = qw(Gilligan Skipper Ginger Professor Mary Ann);
 
-sub sortie {
-	my $biggest = shift @_;
-	my @sortedlist;
-	foreach my $num (@_) {
-		if ($num > $biggest) {
-			$biggest = $num;
-			push @sortedlist, $biggest;
-		}
-	}
-	print "Biggest is $biggest\n";
-	print "Sorted list is @sortedlist\n";
+my %seen = map { $_ , 1 } @castaways;
+print "Gilligan is present\n" if ($seen{'Gilligan'});
 
-}
+my @input_numbers = (1,2,4,8,16,32,64);
 
-&sortie(@arr);
+my @seperated = map { split // } @input_numbers;
+print "@seperated\n";
+
+my @chars = map { split // } @castaways;
+print "@chars\n";
+
